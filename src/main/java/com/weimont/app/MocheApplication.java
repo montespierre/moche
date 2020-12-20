c5c2d34adca064e6557d6d002dada0187ebeae1d
Tu Nombre <montespierre@gmail.com>
2020-12-19 23:06:07 -0500

    nuevo

---
+++ src/main/java/com/weimont/app/MocheApplication.java
@@ -0,0 +1,13 @@
+package com.weimont.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocheApplication.class, args);
+	}
+
+}
