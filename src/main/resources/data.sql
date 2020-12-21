339677177933535438a98eb1e3f79db44883b622
Tu Nombre <montespierre@gmail.com>
2020-12-21 18:56:15 -0500

    d

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,15 @@
-insert into empleados(nomEmp ,puestoEmp , telEmp) 
+create database d6b891bdsbvnn6;
+
+create table empleados
+(
+	idEmpleado int(11) not null
+		constraint users_table_pk
+			primary key,
+	nomEmp varchar(50),
+	puestoEmp varchar(50),
+	telEmp int(11)
+);
+
+insert into empleados(idEmpleado ,nomEmp ,puestoEmp , telEmp) 
 values('1','Juan','jefe','55450');
 
