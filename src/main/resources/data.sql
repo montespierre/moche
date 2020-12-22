6212cb5349beb3c14c0348c066b6772823a30490
Tu Nombre <montespierre@gmail.com>
2020-12-21 19:03:16 -0500

    heroku con db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,12 @@
+DROP TABLE IF EXISTS empleados;
 create table empleados
 (
-	idEmpleado int(11) not null
-		constraint users_table_pk
-			primary key,
+	idEmpleado SERIAL PRIMARY KEY,
 	nomEmp varchar(50),
 	puestoEmp varchar(50),
 	telEmp int(11)
 );
 
-insert into empleados(idEmpleado ,nomEmp ,puestoEmp , telEmp) 
-values('1','Juan','jefe','55450');
+insert into empleados(nomEmp ,puestoEmp , telEmp) 
+values('Juan','jefe','55450');
 
