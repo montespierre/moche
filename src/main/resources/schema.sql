baa84a9f1f6759f312e753e0e4a5a6f529eacabc
Tu Nombre <montespierre@gmail.com>
2020-12-22 20:25:48 -0500

    t

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS empleados;
 CREATE TABLE empleados(
 	idEmpleado SERIAL PRIMARY KEY,
-	nomEmp VARCHAR(50),
-	puestoEmp VARCHAR(50),
+	nomEmp VARCHAR(255),
+	puestoEmp VARCHAR(255),
 	telEmp INTEGER
 	);
