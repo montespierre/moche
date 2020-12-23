18e0ba287f51cbfaf8ebbd79a55ba05b20d486bf
Tu Nombre <montespierre@gmail.com>
2020-12-22 19:00:10 -0500

    eee

--- src/main/java/com/weimont/app/entity/Empleado.java
+++ src/main/java/com/weimont/app/entity/Empleado.java
@@ -2,6 +2,8 @@ package com.weimont.app.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,6 +13,7 @@ public class Empleado {
 
 	@Id
 	@Column(name="idEmpleado")
+	@GeneratedValue(strategy= GenerationType.AUTO)
 	private Long idEmpleado;
 	
 	@Column(name="nomEmp")
